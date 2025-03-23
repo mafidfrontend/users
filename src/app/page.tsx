58cf4b01d0a917240a418d7711bb7836d2da8f50
mafidfrontend <neo.uz001@gmail.com>
2025-03-23 11:40:38 +0500

    loading with another file / product datas with mock api / type file added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,18 @@ import Products from "@/components/Products";
 
 export default function Home() {
     return (
-        <Products />
+        <div className="container mx-auto">
+            <div>
+                <img
+                    style={{
+                        height: 500,
+                        width: "100%",
+                    }}
+                    src="/banner.jpg"
+                    alt=""
+                />
+            </div>
+            <Products />
+        </div>
     );
 }
--- src/components/Products.tsx
+++ src/components/Products.tsx
@@ -1,61 +1,61 @@
 "use client";
 import { ProductsDatasType } from "@/types";
-import { LoadingOutlined } from "@ant-design/icons";
 import { message } from "antd";
 import axios from "axios";
 import { useEffect, useState } from "react";
+import Loading from "./Loading";
 
 export default function Products() {
     const [productsData, setProductsData] = useState<ProductsDatasType[]>([]);
-    const [loading, setLoading] = useState(false)
+    const [loading, setLoading] = useState(false);
     useEffect(() => {
-        setLoading(true)
+        setLoading(true);
         axios
             .get(
                 "https://67df9d1f7635238f9aaa0ed7.mockapi.io/mahsulotlar/product/product"
             )
             .then((response) => {
                 console.log(response.data);
-                setProductsData(response.data)
-                setLoading(false)
-            }).catch(() => {
-                setLoading(false)
-                message.error("Xatolik yuzaga keldi")
+                setProductsData(response.data);
+                setLoading(false);
             })
+            .catch(() => {
+                setLoading(false);
+                message.error("Xatolik yuzaga keldi");
+            });
     }, []);
 
-    if(loading) {
-        return <LoadingOutlined />
+    if (loading) {
+        return (
+            <div className="w-full h-[100vh] flex mx-auto">
+                <Loading />
+            </div>
+        );
     }
 
     return (
-        <div className="container mx-auto">
-            <div>
-                <img
-                    style={{
-                        height: 500,
-                        width: "100%",
-                    }}
-                    src="/banner.jpg"
-                    alt=""
-                />
-            </div>
-            <div className="flex flex-wrap justify-between mt-10 px-20">
-                {productsData.map((product, index) => {
-                    return (
-                        <div key={index} className="mb-3">
-                            <div>
-                                <img src="https://fakeimg.pl/300/" alt={product.productAbout} />
-                            </div>
-                            <h3 className="font-bold">{product.productName}</h3>
-                            <p>{product.productAbout}</p>
-                            <p>Rating: {product.productRating}</p>
-                            <p>Narxi: {product.productPrice}</p>
-                            <span>Soni: {product.productCount}</span>
+        <div className="flex flex-wrap justify-between mt-10 px-20">
+            {productsData.map((product, index) => {
+                return (
+                    <div key={index} className="mb-3">
+                        <div>
+                            <img
+                                src="https://fakeimg.pl/300/"
+                                alt={product.productAbout}
+                            />
                         </div>
-                    )
-                })}
-            </div>
+                        <div className="px-5">
+                            <h3 className="font-bold capitalize mt-3">
+                                {product.productName}
+                            </h3>
+                            <p className="font-bold capitalize mb-3">{product.productAbout}</p>
+                            <p>Rating: {product.productRating}</p>
+                            <p>Narxi: {product.productPrice} so'm</p>
+                            <span>Soni: {product.productCount} dona</span>
+                        </div>
+                    </div>
+                );
+            })}
         </div>
     );
 }
